b49fb974296c72cbbb1b12f455aeeed4040947d7
Ryan Freude <freudehack@gmail.com>
2017-07-28 14:00:18 -0500

    auth strategy implemented, need to fix mismatch uri

--- server/index.js
+++ server/index.js
@@ -1,15 +1,29 @@
 const express = require('express');
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oath20').Strategy
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-//set up passport with some parameters
-passport.use(new GoogleStrategy())
+//set up passport with our client parameters
+//and a callback url to redirect users back to our server
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, accessToken => {
+    console.log (accessToken);
+  })
+);
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+// 'google' calls GoogleStrategy above
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
