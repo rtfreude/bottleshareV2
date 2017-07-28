a7a67bc473c982c5129d7ef47a0024420409cd0b
Ryan Freude <freudehack@gmail.com>
2017-07-28 13:11:35 -0500

    began passport inegration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oath20').Strategy
 
 const app = express();
+
+//set up passport with some parameters
+passport.use(new GoogleStrategy())
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
